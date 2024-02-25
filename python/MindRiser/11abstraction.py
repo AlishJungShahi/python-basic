e0f0eafcb11d9d118105c5fa3bd63e7fbd18a467
alishshahi27@ <alishshahi27@gmail.com>
2024-02-25 20:07:13 +0545

    new changes

---
+++ python/MindRiser/11abstraction.py
@@ -0,0 +1,43 @@
+# # #%%
+# # while True:
+# #     try:
+# #         a = int(input('Enter first number'))
+# #         b = int(input('Enter your second number'))
+        
+# #         print(a/b)
+        
+# #     except ZeroDivisionError:
+# #         print('A number is cannot divisiable by zero')
+        
+# #     # except ValueError:
+# #     #     print('String cannot support')
+# #     #find error itself    
+# #     except Exception as e:
+# #         print('Something went wrong',e)
+        
+        
+# # #%%
+# # while True:
+# #     try:
+# #         a = int(input('Enter first number'))
+# #         b = int(input('Enter second number'))
+        
+# #         if b == 5:
+# #             raise Exception ('It cannot support')
+            
+# #     except Exception as e:
+# #         print('Somthing wrong',e)
+        
+        
+# # # %%
+# # while true:
+# #     number = [1,2,3,4,5,6,7,8,9,10,11]
+# #     index = int(input('Enter number'))
+# #     print(number[index])
+#  # %%
+# # while True:
+# #     number = [1,2,3,4,5,6,7,8,9,10,11]
+# #     index = int(input('Enter number'))
+# #     print(number[index])
+
+
